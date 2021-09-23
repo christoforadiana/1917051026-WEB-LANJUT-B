49b0b5beba97bb587730035b3552d4501726dff1
christoforadiana <christoforadianayuliawati@gmail.com>
2021-09-23 15:07:01 +0700

    penambahan file pada folder app

---
+++ app/Controllers/Templating.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Templating extends BaseController
+{
+    public function index()
+    {
+        $data = ['title' => 'Blog - Post'];
+        return view('view_admin');
+    }
+}
